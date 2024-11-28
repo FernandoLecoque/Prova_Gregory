3ae8e20beed6c16e7a50e68d92eb323fc08c75c2
Fernando <168545421+FernandoLecoque@users.noreply.github.com>
2024-11-28 11:27:42 -0300

    INICIADO E FINALIZADO

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// Importação do bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Importação estilo CSS padrão
+import "./App.css";
+
+// Importação de páginas
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import CadastroProduto from "./pages/CadastroProduto";
+import EditarProduto from "./pages/EditarProduto";
+
+//Importação do gerenciador de rotas
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}/>
+          <Route path="/login" element={<Login />}/>  
+          <Route path="/home" element={<Home />}/> 
+          <Route path="/produto/cadastrar" element={<CadastroProduto />}/>  
+          <Route path="/produto/editar/:id" element={<EditarProduto />}/>     
+        </Routes>
+      </BrowserRouter>
+    </div> 
+  );
+}
+
+export default App;
